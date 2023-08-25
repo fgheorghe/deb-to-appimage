e6e953f58dcd77fd88723dd4afe7c14e55ebe597
Flaviu Gheorghe Grosan <fgheorghe@grosan.co.uk>
2023-08-25 23:16:10 +0100

    Removed broken examples.

--- download-dependencies.sh
+++ download-dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PACKAGE_NAME=$1
 cd $2 && \
-apt-cache depends --recurse -o APT::Cache::ShowOnlyFirstOr=true --no-recommends --no-suggests --no-breaks --no-suggests --no-replaces --no-conflicts --no-pre-depends -i ${PACKAGE_NAME} |
+apt-cache depends --recurse -o APT::Cache::ShowOnlyFirstOr=true --no-recommends --no-breaks --no-suggests --no-replaces --no-conflicts --no-pre-depends -i ${PACKAGE_NAME} |
 grep -v "<" |
 awk '/Depends:/ {print $2}' |
 sort |
